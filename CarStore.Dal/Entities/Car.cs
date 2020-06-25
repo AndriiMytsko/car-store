c1493e8fe214c276bfdab58f7f22b29537f4bc11
“Andrii <mytsko.andrii@gmail.com>
2020-06-25 17:35:17 +0300

    Added base level

---
+++ CarStore.Dal/Entities/Car.cs
@@ -0,0 +1,8 @@
+﻿namespace CarStore.Dal.Entities
+{
+    public class Car : Entity
+    {
+        public string Marka { get; set; }
+        public string Model { get; set; }
+    }
+}
---
+++ CarStore.Dal/Entities/Entity.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace CarStore.Dal.Entities
+{
+    public class Entity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdatedAt { get; set; }
+    }
+}
