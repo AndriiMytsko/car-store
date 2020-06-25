7c9ddd359891fc268b4b8044cad4f16bdc8d7781
“Andrii <mytsko.andrii@gmail.com>
2020-06-25 18:05:45 +0300

    Getting Swagger

--- CarStore.WebApi/Startup.cs
+++ CarStore.WebApi/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 
 namespace CarStore.WebApi
 {
@@ -28,6 +29,11 @@ namespace CarStore.WebApi
                 .AddEfDal(Configuration)
                 .AddMapper()
                 .AddMvcCore();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -39,6 +45,12 @@ namespace CarStore.WebApi
             }
 
             app.UseHttpsRedirection();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseRouting();
 
